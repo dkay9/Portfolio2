c3748ae206062ea0c1c3ba301857619883a77c06
Kalu David <kaludavid411@gmail.com>
2024-10-14 13:26:32 +0100

    another commit

--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ let typed = new Typed('.typing', {
     BackSpeed: 60,
     loop: true
 })
+/* =============== Aside =============== */
+const nav = document.querySelector('.nav'),
+      navList = nav.querySelectorAll('li'),
+      totalNavList = navList.length;
+      for (let i = 0; i < totalNavList; i++){
+        const a = navList[i].querySelector('a')
+        a.addEventListener('click', function(){
+            for(let j = 0; j < totalNavList; j++){
+                const a = navList[j].querySelector('a').classList.remove('active')
+            }
+            this.classList.add('active')
+            showSection(this)
+        })
+      }
+      function showSection(element){
+        const target = element.getAttribute('href').split('#')[1]
+        document.querySelector('#' + target).classList.add('active')
+        
+      }
