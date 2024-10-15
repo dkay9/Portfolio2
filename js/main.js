c706dc95b32c2754014237dc119a05001e7de9b6
Kalu David <kaludavid411@gmail.com>
2024-10-15 20:49:14 +0100

    another commit

--- js/main.js
+++ js/main.js
@@ -25,6 +25,9 @@ const nav = document.querySelector('.nav'),
             }
             this.classList.add('active')
             showSection(this)
+            if(window.innerWidth < 1200){
+                asideSectionTogglerBtn()
+            }
         })
       }
       function showSection(element){
@@ -43,4 +46,7 @@ const nav = document.querySelector('.nav'),
             function asideSectionTogglerBtn(){
                 aside.classList.toggle('open')
                 navTogglerBtn.classList.toggle('open')
+                for(let i = 0; i < totalSection; i++){
+                    allSection[i].classList.toggle('open')
+                }
             }
